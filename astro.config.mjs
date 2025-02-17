411e17f5d976093b1cfeed67be98eec21e01c0c1
stherlt <151394883+stherlt@users.noreply.github.com>
2025-02-17 09:51:24 -0700

    Fixed logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
+  base: '/'
 });
